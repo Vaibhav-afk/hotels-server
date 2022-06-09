bed2b6d9a48462ef6092885f58950f32e9024aed
Vaibhav-afk <vbhvka@gmail.com>
2022-06-09 13:03:10 +0530

    make it better

--- index.js
+++ index.js
@@ -21,10 +21,10 @@ mongoose
 // register swagger
 fastify.register(require("@fastify/swagger"), swagger.options);
 
-// // server test route
-// fastify.get("/", function (req, reply) {
-//   reply.send({ message: "hotel server is running!!!!" });
-// });
+// server test route
+fastify.get("/", function (req, reply) {
+  reply.send({ message: "hotel server is running!!!!" });
+});
 
 routes.forEach((route, index) => {
   fastify.route(route);
@@ -33,7 +33,7 @@ routes.forEach((route, index) => {
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen(8000 || process.env.PORT, "0.0.0.0");
+    await fastify.listen(process.env.PORT || 8000);
     fastify.swagger();
     fastify.log.info(`server listening on ${fastify.server.address().port}`);
   } catch (err) {
